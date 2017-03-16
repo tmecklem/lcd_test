c6fe50cd81837d1c57bc8e8f7f90e8b88bfb52ae
Tim Mecklem <timothy@mecklem.com>
2017-03-15 22:09:39 -0400

    Upgrade to nerves 0.5.0

--- mix.exs
+++ mix.exs
@@ -1,37 +1,50 @@
 defmodule LcdTest.Mixfile do
   use Mix.Project
 
-  @target System.get_env("NERVES_TARGET") || "rpi"
-
+  @target System.get_env("MIX_TARGET") || "host"
+  Mix.shell.info([:green, """
+  Env
+  MIX_TARGET:   #{@target}
+  MIX_ENV:      #{Mix.env}
+  """, :reset])
   def project do
     [app: :lcd_test,
      version: "0.0.1",
+     elixir: "~> 1.4.0",
      target: @target,
-     archives: [nerves_bootstrap: "0.1.2"],
+     archives: [nerves_bootstrap: "~> 0.3.0"],
      deps_path: "deps/#{@target}",
      build_path: "_build/#{@target}",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     aliases: aliases,
-     deps: deps ++ system(@target)]
+     aliases: aliases(@target),
+     deps: deps()]
   end
 
-  def application do
-    [applications: [:nerves, :logger, :elixir_lcd, :elixir_matrix_keypad, :elixir_ale],
-     mod: {LcdTest, []}]
+  def application, do: application(@target)
+  def application("host") do
+    [extra_applications: [:logger]]
+  end
+  def application(_target) do
+    [mod: {LcdTest.Application, []},
+     extra_applications: [:logger, :elixir_lcd, :elixir_matrix_keypad, :elixir_ale]]
   end
 
   defp deps do
-    [{:nerves, "~> 0.3.0"},
-     {:elixir_lcd, path: "/Users/tmecklem/src/nerves/elixir_lcd"},
-     {:elixir_matrix_keypad, path: "/Users/tmecklem/src/nerves/elixir_matrix_keypad"}]
+    [{:nerves, "~> 0.5.0", runtime: false}] ++
+      deps(@target)
   end
 
-  def system(target) do
-    [{:"nerves_system_#{target}", ">= 0.0.0"}]
+  def deps("host"), do: []
+  def deps(target) do
+    [{:nerves_runtime, "~> 0.1.0"},
+     {:"nerves_system_#{target}", "~> 0.11.0", runtime: false},
+     {:elixir_lcd, github: "tmecklem/elixir_lcd"},
+     {:elixir_matrix_keypad, github: "tmecklem/elixir_matrix_keypad"}]
   end
 
-  def aliases do
+  def aliases("host"), do: []
+  def aliases(_target) do
     ["deps.precompile": ["nerves.precompile", "deps.precompile"],
      "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
   end
